2026833d18312bf932a7a7ec8ed3da65baae8349
Yugendra Ray <ray.yugen@gmail.com>
2023-08-08 07:12:24 +0545

    firt scrapping commit

---
+++ main.py
@@ -0,0 +1,39 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = "https://www.flipkart.com/clothing-and-accessories/topwear/shirt/men-shirt/formal-shirt/pr?sid=clo,ash,axc,mmk,bk1&otracker=categorytree&otracker=nmenu_sub_Men_0_Formal%20Shirts"
+r = requests.get(url)
+htmlContent = r.content
+
+soup = BeautifulSoup(htmlContent, "html.parser")
+
+# To get title of the HTML page
+title  =soup.title
+# print(title.string)
+
+# Commonly used types of objects in beatifulsoup
+# print(type(title.string)) -- Navigable String
+# print(type(title)) -- Tag
+# print(type(soup)) -- BeautifulSoup
+# 4. Comment
+
+# Print all anchor tags
+anchor_tags = soup.find_all("a")
+# print(anchor_tags)
+
+# Print all p tags
+paragraphs = soup.find_all("p")
+# print(paragraphs)
+print(soup.get_text())
+
+
+# To print first paragraph
+# print(soup.find("p"))
+
+# To get text only
+# print(soup.find("p").get_text())
+
+# To print all the paragraph with given class name
+# print(soup.find_all("p", class_="_2-N8zT"))
+
+
